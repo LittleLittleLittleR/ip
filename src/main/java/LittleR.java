4642b8a030d39d717dfc5e199db08cb48ba7bf37
RaphaelYeo <raphaelclashacc3@gmail.com>
2026-08-19 13:36:55 +0800

    Add greet and exit text

--- src/main/java/LittleR.java
+++ src/main/java/LittleR.java
@@ -1,5 +1,14 @@
 public class LittleR {
     public static void main(String[] args) {
+        printLineBreak();
+        printBanner();
+        printWelcome();
+        printLineBreak();
+        printGoodbye();
+        printLineBreak();
+    }
+
+    public static void printBanner() {
         String banner = "  _         _      _       _        _              ___ \n"
                 + " │ │       (_)    │ │_    │ │_     │ │     ___    │ _ \\\n"
                 + " │ │__     │ │    │  _│   │  _│    │ │    / ─_)   │   /\n"
@@ -7,5 +16,21 @@ public class LittleR {
                 + "_│\"\"\"\"\"│_│\"\"\"\"\"│_│\"\"\"\"\"│_│\"\"\"\"\"│_│\"\"\"\"\"│_│\"\"\"\"\"│_│\"\"\"\"\"│\n"
                 + "\"`─0─0─'\"`─0─0─'\"`─0─0─'\"`─0─0─'\"`─0─0─'\"`─0─0─'\"`─0─0─'\n";
         System.out.println(banner);
+    }
+
+    public static void printWelcome() {
+        String welcomeMessage = "Hello! I'm LittleR\n"
+                + "What can I do for you?\n";
+        System.out.println(welcomeMessage);
+    }
+
+    public static void printGoodbye() {
+        String goodbyeMessage = "Bye. Hope to see you again soon!\n";
+        System.out.println(goodbyeMessage);
+    }
+
+    public static void printLineBreak() {
+        String lineBreak = "____________________________________________________________\n";
+        System.out.println(lineBreak);
     }
 }
